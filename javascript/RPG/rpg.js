6ac15f20be6188acb10cb7d46baa6ccbd6eaf116
mestre <kadukitesesi@gmail.com>
2023-11-12 23:44:17 -0300

    Escolha de dado

--- javascript/RPG/rpg.js
+++ javascript/RPG/rpg.js
@@ -4,13 +4,30 @@ jogador1 = ["Kokori",20,20];
 
 jogador2 = ["inai",20,20];
 
+var escolhaJogador = document.getElementById("escolhaJogador").value;
+var escolhaDado = document.getElementById("escolhaDado").value
+
 function girarDado(){
+
     var dado = Math.floor(Math.random()*20);
     document.getElementById("sorte").innerHTML = dado;
+    var dado8 = Math.floor(Math.random()*8);
+    document.getElementById("sorte").innerHTML = dado8;
+    
 
     if(dado < 10){
         document.getElementById("nota").innerHTML = "Você se deu mal"
     }else{
         document.getElementById("nota").innerHTML = "Parabéns"
+    }
+    switch(escolha){
+        case"Inai":
+            var dano = jogador2[1] - dado8
+            document.getElementById("sorte").innerHTML = dano
+        break
+        case"Kokori":
+            var dano = dado8 - jogador1[1]
+            document.getElementById("sorte").innerHTML = dano
+        break
     }
 }
