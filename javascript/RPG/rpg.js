a631bf5726bf79902bb018f6d015440be72f11d2
Carlos <kadukitesesi@gmail.com>
2023-11-14 18:24:35 -0300

    Tentando resolver bug do d8

--- javascript/RPG/rpg.js
+++ javascript/RPG/rpg.js
@@ -11,33 +11,39 @@ var inimigos = [
     {nome: "REX", especie:"Ogro", vida:80}
 ];
 
+var dados = [
+    {nome: "d20",numero: 20},
+    {nome: "d8", numero:8},
+    {nome:"d12", numero:12},
+    {nome:"d2",numero:2}
+];
+
 var escolhaJogador = document.getElementById("escolhaJogador").value;
-var escolhaDado = document.getElementById("escolhaDado").value
-var acao = document.getElementById("acaoJogador").value
-var inimigoSelect = document.getElementById("inimigos").value
+var escolhaDado = document.getElementById("escolhaDado").value;
+var acao = document.getElementById("acaoJogador").value;
+var inimigoSelect = document.getElementById("inimigos").value;
 
 
 
 function girarDado(){
     switch(escolhaDado){
         case"d20":
-             var dado = Math.floor(Math.random()*20);
-            document.getElementById("sorte").innerHTML = dado
+             var dado20 = Math.floor(Math.random()*dados[0].numero);
+            document.getElementById("sorte").innerHTML = dado20
         break
         case"d8":
-        var dado8 = Math.floor(Math.random()*8);
+        var dado8 = Math.floor(Math.random()*dados[1].numero);
         document.getElementById("nota").innerHTML = dado8    
         break
         }
         switch(escolha){
         case"Inai":
-            if(acao == "ataque"){
-                var dano = inimigos[0].vida - dado
+            
+               // var dano = inimigos[0].vida - dado
                 document.getElementById().innerHTML = inimigos[0].vida
-            }
         break
         case"Kokori":
-            var dano = dado - jogadores_rpg[0].vida
+        //    var dano = dado - jogadores_rpg[0].vida
             document.getElementById().innerHTML = dano
         break
     }
