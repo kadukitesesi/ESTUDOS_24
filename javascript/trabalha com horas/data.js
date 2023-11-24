6b70202a2f54b91d5ad15be6279c585a22d8b9de
Carlos <kadukitesesi@gmail.com>
2023-11-24 15:03:19 -0300

    testes

--- javascript/trabalha com horas/data.js
+++ javascript/trabalha com horas/data.js
@@ -3,9 +3,9 @@ const hora = new Date().getHours();
 
 if(hora < 5 || hora >= 18){
     document.getElementById('body').style.backgroundColor = '#32cd32';
-    document.getElementById('frase').innerHTML = 'Cala a boca trabalho eu tô vadiando porra'
+    document.getElementById('frase').innerHTML = 'Cala a boca trabalho, eu tô vadiando porra'
 }else if(hora >= 5 || hora < 18){
-    document.getElementById('frase').innerHTML = 'Cala a boca vadia eu tô trabalhando porra'
+    document.getElementById('frase').innerHTML = 'Cala a boca vadia, eu tô trabalhando porra'
     document.getElementById('body').style.backgroundColor = '#111111'
     document.getElementById('body').style.color = 'white'
 }
