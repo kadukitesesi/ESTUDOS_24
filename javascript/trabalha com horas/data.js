e3ee7ac33ac1bc4d434f43eae10847270dae281c
mestre <kadukitesesi@gmail.com>
2023-11-23 22:24:46 -0300

    mini projeto de horas

---
+++ javascript/trabalha com horas/data.js
@@ -0,0 +1,11 @@
+const hora = new Date().getHours();
+
+
+if(hora < 5 || hora >= 18){
+    document.getElementById('body').style.backgroundColor = '#32cd32';
+    document.getElementById('frase').innerHTML = 'Cala a boca trabalho eu tô vadiando porra'
+}else if(hora >= 5 || hora < 18){
+    document.getElementById('frase').innerHTML = 'Cala a boca vadia eu tô trabalhando porra'
+    document.getElementById('body').style.backgroundColor = '#111111'
+    document.getElementById('body').style.color = 'white'
+}
