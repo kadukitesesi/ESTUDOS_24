8de01dc17423dae6d9d4b35cee75fa2701c7d5fd
mestre <kadukitesesi@gmail.com>
2023-11-27 17:42:35 -0300

    adicionar armazenamento de dados

--- javascript/lista de tarefas/main.js
+++ javascript/lista de tarefas/main.js
@@ -1,12 +1,19 @@
-let arrayTarefas = []
+let hoje = new Date();
 
-function criaTarefa(){
-    let conteudo = document.getElementById("tarefa").value
-    let selectLista = document.getElementById("lista")
-    let novaTarefa = document.createElement("li")
+function criaTarefa() {
+    let conteudo = document.getElementById("tarefa").value;
+    let selectLista = document.getElementById("lista");
+    let novaTarefa = document.createElement("li");
+
+    let horaTarefa = hoje.getHours();
+    let minutoTarefa = hoje.getMinutes();
+    let momentoTarefa = `${horaTarefa}:${minutoTarefa}`;
 
     novaTarefa.textContent = conteudo;
     selectLista.appendChild(novaTarefa);
 
-
+    let dataElement = document.createElement("span");
+    dataElement.textContent = momentoTarefa;
+    dataElement.className = "data-tarefa";
+    novaTarefa.appendChild(dataElement);
 }
